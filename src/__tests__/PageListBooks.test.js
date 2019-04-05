a1ba676d2ecf77c957d6082a237a5e3584f17f22
Antônio Rodrigues <antonio.rodrigues_tga@hotmail.com>
2019-04-05 17:14:21 -0300

    test: Adding PageListBooks.test.js

---
+++ src/__tests__/PageListBooks.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import PageListBooks from '../components/PageListBooks';
+import books from '../mocks/Books';
+
+const setup = {
+  books,
+  handleUpdateBook: jest.fn(),
+  onChangePage: { push: jest.fn() }
+};
+
+it('renders PageListBooks component without crashing', () => {
+  shallow(<PageListBooks {...setup} />);
+});
